57d05d77f9783443a1f3f2ec60641e4dd0480ffd
Synth <reyhanacynthia@gmail.com>
2023-04-14 15:20:29 +0300

    Home complete

--- global-domestic-workers/src/app/app.module.ts
+++ global-domestic-workers/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
